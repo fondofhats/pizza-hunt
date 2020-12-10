6749e17932609650e741fe6e02de24b01e1a057f
Michael Giddings <mikegiddings@gmail.com>
2020-12-09 21:43:59 -0500

    added comment functionality

--- models/Pizza.js
+++ models/Pizza.js
@@ -1,24 +1,44 @@
-const {Schema, model} = require('mongoose');
+const { Schema, model } = require("mongoose");
+const dateFormat = require("../utils/dateFormat");
 //const { String } = require('sequelize/types');
 
-const PizzaSchema = new Schema({
+const PizzaSchema = new Schema(
+  {
     pizzaName: {
-        type: String
+      type: String,
     },
     createdBy: {
-        type: String
+      type: String,
     },
     createdAt: {
-        type: Date,
-        default: Date.now
+      type: Date,
+      default: Date.now,
+      get: (createdAtVal) => dateFormat(createdAtVal)
     },
     size: {
-        type: String,
-        default: 'Large'
+      type: String,
+      default: "Large",
     },
-    toppings: []
-});
+    toppings: [],
+    comments: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Comment",
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true
+    },
+    id: false,
+  }
+);
 
-const Pizza = model('Pizza', PizzaSchema);
+PizzaSchema.virtual("commentCount").get(function () {
+  return this.comments.length;
+});
+const Pizza = model("Pizza", PizzaSchema);
 
 module.exports = Pizza;
