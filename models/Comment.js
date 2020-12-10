8e8be71b181794d02e7eb4fc17bd6dd0f9df1b4f
Michael Giddings <mikegiddings@gmail.com>
2020-12-10 00:14:18 -0500

    added reply feature

--- models/Comment.js
+++ models/Comment.js
@@ -1,18 +1,54 @@
-const { Schema, model} = require('mongoose');
+const { Schema, model, Types } = require("mongoose");
+const dateFormat = require("../utils/dateFormat");
 
-const CommentSchema = new Schema ({
-    writtenBy: {
-        type: String
-    },
-    commentBody: {
-        type: String
-    },
-    createdAt: {
-        type: Date,
-        default: Date.now
+const ReplySchema = new Schema({
+  replyId: {
+    type: Schema.Types.ObjectId,
+    default: () => new Types.ObjectId()
+  },
+  replyBody: {
+    type: String,
+  },
+  writtenBy: {
+    type: String,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    get: createdAtVal => dateFormat(createdAtVal),
+  },
+},
+{
+    toJSON: {
+        getters: true
     }
 });
 
-const Comment = model('Comment', CommentSchema);
+const CommentSchema = new Schema({
+  writtenBy: {
+    type: String,
+  },
+  commentBody: {
+    type: String,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    get: createAtVal => dateFormat(createAtVal)
+  },
+  replies: [ReplySchema]
+},
+{
+    toJSON: {
+        getters: true
+    },
+    id: false
+});
+
+CommentSchema.virtual('replyCount').get(function(){
+    return this.replies.length;
+});
+
+const Comment = model("Comment", CommentSchema);
 
 module.exports = Comment;
